75964fbdb9e05aa7638b88dcaf96e9180a37fdc6
Freddy PC <freddythepc@gmail.com>
2024-02-06 20:45:20 -0500

    style: positioned elements and added routing to image

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,26 @@
 import React from "react";
 import "./Footer.css";
+import LinkedIn from "../../images/linkedinLogo.svg";
 
 const Footer = () => {
+  const today = new Date();
+  const handleClick = () => {
+    // Navigate to the LinkedIn profile when the image is clicked
+    window.open("https://www.linkedin.com/in/freddy-perez-camacho", "_blank");
+  };
+
   return (
     <footer className="footer">
       <div className="footer__container">
-        <p>footer</p>
+        <p className="footer__text">
+          @ {today.getFullYear()} Developed by Freddy Perez-Camacho
+        </p>
+        <img
+          src={LinkedIn}
+          className="footer__image"
+          alt="linkedIn link"
+          onClick={handleClick}
+        />
       </div>
     </footer>
   );
