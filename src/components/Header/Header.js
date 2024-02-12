d79d7f1931277b4affa36499fd7f9b9e8a98c827
Freddy PC <freddythepc@gmail.com>
2024-02-12 18:45:28 -0500

    style: add template for header design

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,39 +17,53 @@ const Header = () => {
         <NavLink to="/">
           <img src={logoImage} alt="Weather Logo" className="header__logo" />
         </NavLink>
-      </div>
-      <div className="header__navmobile">
-        <div
-          className={`navmobile__container ${isMenuOpen ? "active" : ""}`}
-          onClick={toggleMenu}
-        >
-          <div className="navmobile__line"></div>
-          <div className="navmobile__line"></div>
-          <div className="navmobile__line"></div>
+        <div className="header__navmobile">
+          <div
+            className={`navmobile__container ${isMenuOpen ? "active" : ""}`}
+            onClick={toggleMenu}
+          >
+            <div className="navmobile__line"></div>
+            <div className="navmobile__line"></div>
+            <div className="navmobile__line"></div>
+          </div>
         </div>
+        <ul className={`headernav__links ${isMenuOpen ? "active" : ""}`}>
+          <li>
+            <a href="#services" className="headernav__link">
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#gallary" className="headernav__link">
+              Gallary
+            </a>
+          </li>
+          <li>
+            <a href="#aboutus" className="headernav__link">
+              About Us
+            </a>
+          </li>
+          <li>
+            <a href="#contacts" className="headernav__link">
+              Contacts
+            </a>
+          </li>
+        </ul>
       </div>
-      <ul className={`headernav__links ${isMenuOpen ? "active" : ""}`}>
-        <li>
-          <a href="#services" className="headernav__link">
-            Services
-          </a>
-        </li>
-        <li>
-          <a href="#gallary" className="headernav__link">
-            Gallary
-          </a>
-        </li>
-        <li>
-          <a href="#aboutus" className="headernav__link">
-            About Us
-          </a>
-        </li>
-        <li>
-          <a href="#contacts" className="headernav__link">
-            Contacts
-          </a>
-        </li>
-      </ul>
+      <div className="header__content">
+        <h1 className="header__title">Welcome to A&C Painting</h1>
+        <p className="header__text">
+          With over 10 years of experience in the painting industry, we deliver
+          high-quality services at an affordable price. Feel free to ask us any
+          questions about your next project.
+        </p>
+      </div>
+      <div className="header__footer">
+        <a href="#contacts" className="header__link">
+          How to Contact Us 🤔❓
+        </a>
+        <p className="header__location">Asheville, North Carolina</p>
+      </div>
     </header>
   );
 };
