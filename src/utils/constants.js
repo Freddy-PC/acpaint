ae8a2ac1c3989e32fddb18f572e693ab559fe468
Freddy PC <freddythepc@gmail.com>
2024-02-14 20:36:43 -0500

    refactor: add array of objects for cards

---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+// import all images used for the array of objects below
+// import image from "../images/brightnight.svg";
+import hoseLogo from "../images/hoseLogo.png";
+
+const servicesData = [
+  { title: "Pressure Washing", text: "text here", image: hoseLogo },
+  //   { title: "Exterior Painting", text: "text here" },
+  //   { title: "Interior Painting", text: "text here" },
+  //   { title: "Additional Painting", text: "text here" },
+  //   { title: "Stain", text: "text here" },
+  //   { title: "Primer", text: "text here" },
+  //   { title: "Popcorn Removal", text: "text here" },
+];
+
+export { servicesData };
