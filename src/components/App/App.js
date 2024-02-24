d02eb871d8ff0c763700dd02c7f27f81b0e84d6a
Freddy PC <freddythepc@gmail.com>
2024-02-23 22:22:49 -0500

    refactor: pass props to Nav and header components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
-import Header from "../Header/Header";
+import Navigation from "../Navigation/Navigation";
 import Main from "../Main/Main";
 import Gallary from "../Gallary/Gallary";
 import Footer from "../Footer/Footer";
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+  // guide user in main page
+  const scrollToSection = (sectionId) => {
+    setTimeout(() => {
+      const servicesSection = document.getElementById(sectionId);
+      if (servicesSection) {
+        console.log(servicesSection);
+        servicesSection.scrollIntoView({ behavior: "smooth" });
+      } else {
+        console.error(`Element with id ${sectionId} not found.`);
+      }
+    }, 0);
+  };
+
   return (
     <div className="app__page">
-      <Header />
+      <Navigation
+        isMenuOpen={isMenuOpen}
+        setIsMenuOpen={setIsMenuOpen}
+        scrollToSection={scrollToSection}
+        closeMenu={closeMenu}
+      />
+      {/* Move navigation from header to navigation*/}
+      {/* Move Header into Main component*/}
       <Routes>
         <Route path="/gallery" element={<Gallary />} />
         {/* <Route path="/about" element={<About />} /> */}
-        <Route path="/" element={<Main />} />
+        <Route
+          path="/"
+          element={
+            <Main scrollToSection={scrollToSection} closeMenu={closeMenu} />
+          }
+        />
       </Routes>
       <Footer />
     </div>
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Main.css";
+import Header from "../Header/Header";
 import Services from "../../components/Services/Services";
 import Contacts from "../../components/Contacts/Contacts";
 
-function Main() {
+function Main({ scrollToSection, closeMenu }) {
   return (
     <main className="main">
       <div className="main__container">
+        <Header scrollToSection={scrollToSection} closeMenu={closeMenu} />
         <Services />
         <Contacts />
       </div>
