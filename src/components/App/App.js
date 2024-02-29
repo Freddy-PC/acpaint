24c747d7ab2416e06ead8c281c166dc27d150c5b
Freddy PC <freddythepc@gmail.com>
2024-02-29 16:05:34 -0500

    feat: add funct. for modal images, update scroll funct. for gallery, close options for images

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,66 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import Navigation from "../Navigation/Navigation";
 import Main from "../Main/Main";
-import Gallary from "../Gallary/Gallary";
+import Gallery from "../Gallery/Gallery";
 import Footer from "../Footer/Footer";
+import ItemModal from "../ItemModal/ItemModal";
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [selectedCard, setSelectedCard] = useState(null); // equal to image data from array of objects
+  const [activeModal, setActiveModal] = useState(""); // handles the modals
+
+  // hamburger funct.
   const closeMenu = () => {
     setIsMenuOpen(false);
   };
+
   // guide user in main page
   const scrollToSection = (sectionId) => {
     setTimeout(() => {
-      const servicesSection = document.getElementById(sectionId);
-      if (servicesSection) {
-        console.log(servicesSection);
-        servicesSection.scrollIntoView({ behavior: "smooth" });
+      const section = document.getElementById(sectionId);
+      if (section) {
+        const scrollOptions =
+          sectionId === "gallery"
+            ? { behavior: "instant" }
+            : { behavior: "smooth" };
+        section.scrollIntoView(scrollOptions);
       } else {
         console.error(`Element with id ${sectionId} not found.`);
       }
     }, 0);
+  };
+
+  // When image is clicked...
+  const handleClick = (card) => {
+    setSelectedCard(card);
+    setActiveModal(MODAL_TYPE.PREVIEW);
+  };
+
+  const MODAL_TYPE = {
+    PREVIEW: "preview", // image click
+  };
+  const closeAllModals = () => {
+    setActiveModal("");
+  };
+
+  useEffect(() => {
+    const closebyEsc = (evt) => {
+      if (evt.key === "Escape") {
+        closeAllModals();
+      }
+    };
+    window.addEventListener("keydown", closebyEsc);
+    return () => window.removeEventListener("keydown", closebyEsc);
+  }, []);
+
+  const handleOverlay = (e) => {
+    if (e.target === e.currentTarget) {
+      closeAllModals();
+    }
   };
 
   return (
@@ -33,10 +71,8 @@ function App() {
         scrollToSection={scrollToSection}
         closeMenu={closeMenu}
       />
-      {/* Move navigation from header to navigation*/}
-      {/* Move Header into Main component*/}
       <Routes>
-        <Route path="/gallery" element={<Gallary />} />
+        <Route path="/gallery" element={<Gallery cardClick={handleClick} />} />
         {/* <Route path="/about" element={<About />} /> */}
         <Route
           path="/"
@@ -46,6 +82,13 @@ function App() {
         />
       </Routes>
       <Footer />
+      {activeModal === MODAL_TYPE.PREVIEW && selectedCard && (
+        <ItemModal
+          cardData={selectedCard}
+          closeModal={closeAllModals}
+          closeOverlay={handleOverlay}
+        />
+      )}
     </div>
   );
 }
