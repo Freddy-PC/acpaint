e573ea6a730b1076a0593e43af56a07efa477fed
Freddy PC <freddythepc@gmail.com>
2025-02-12 21:09:41 -0500

    feat: change the image if the button oir image is click to the before and after image property

--- src/components/ItemModal/ItemModal.js
+++ src/components/ItemModal/ItemModal.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ItemModal.css";
 import closeButtonLogo from "../../images/closeButtonLogo.png";
 
 // conditionally render the image name for the alt tag
 
+/* if button is clicked, change the image */
+/* if button is clciked, swap between the before and after image*/
+// Handle state of button by setting the useState
+
 const ItemModal = ({ cardData, closeModal, closeOverlay }) => {
+  const [isClicked, setIsClicked] = useState(false);
+  const updateImageState = () => {
+    setIsClicked((previousState) => !previousState);
+  };
+
   return (
     <div className="itemModal" onClick={closeOverlay}>
       <div className="itemModal__container">
@@ -14,8 +23,20 @@ const ItemModal = ({ cardData, closeModal, closeOverlay }) => {
           alt="close button logo"
           onClick={closeModal}
         />
-        <img className="itemModal__image" src={cardData.image} alt="card" />
+        <img
+          className="itemModal__image"
+          src={isClicked ? cardData.imageA : cardData.imageB}
+          alt="card"
+          onClick={updateImageState}
+        />
         <p className="itemModal__description">{cardData.description}</p>
+        {/* Make the button change color CONSTANTLY AND change when hovered 
+        AND change the image */}
+        <div className="itemModal__button-container">
+          <button onClick={updateImageState} className="itemModal__button">
+            BEFORE
+          </button>
+        </div>
       </div>
     </div>
   );
